36c963edefe786c512295c571a24c9dd318ffbfc
Aakash1919 <programmer.aakash@gmail.com>
2021-01-21 13:33:00 -0800

    Interchagend ship to and bill to

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -609,7 +609,7 @@ class Checkout extends Public_Controller{
 
 		$billingAddress = $this->setBillingAddress($body);
 		$shippingAddress = $this->setShippingAddress($body);
-		$emailbody3.=$billingAddress.$shippingAddress;
+		$emailbody3.=$shippingAddress.$billingAddress;
 		$emailbody3.="Notes:<br >".$body['cmnts']."<br ><br >";
 		$emailbody3.="We thank you for your business!<br ><br >Your BioAssay Systems Team<br >";
 		$emailbody=$emailbody1.$emailbody2.$emailbody3;
