ffa80c84872d331c643851527135dfadeaf1bfb3
Aakash1919 <programmer.aakash@gmail.com>
2021-03-18 09:21:38 -0500

    Push from the server

--- application/logs/log-2021-01-16.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2021-01-16 22:34:11 --> Severity: Warning --> mysqli::real_connect(): (HY000/1698): Access denied for user 'c5z2c8v8_bioassa'@'localhost' /var/www/bioassay/system/database/drivers/mysqli/mysqli_driver.php 203
-ERROR - 2021-01-16 22:34:11 --> Unable to connect to the database
-ERROR - 2021-01-16 22:34:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1698): Access denied for user 'c5z2c8v8_bioassa'@'localhost' /var/www/bioassay/system/database/drivers/mysqli/mysqli_driver.php 203
-ERROR - 2021-01-16 22:34:33 --> Unable to connect to the database
