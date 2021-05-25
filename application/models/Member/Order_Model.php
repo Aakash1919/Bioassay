b5ab8a9b95ccffd1ff24f5728cb17ad30f3fe3eb
Aakash1919 <programmer.aakash@gmail.com>
2021-05-26 01:27:45 +0530

    added function in the order model

--- application/models/Member/Order_Model.php
+++ application/models/Member/Order_Model.php
@@ -7,15 +7,28 @@ class Order_Model extends Members_Model{
 		parent::__construct();
 	}
 	    public function Save($id,$data){
-        if($id==NULL){
-            $this->db->insert($this->_table_name,$data);
-            $result = $this->db->insert_id();
-        }else{
-            $this->db->where('orders_id',$id);
-            $this->db->update($this->_table_name,$data);
-            $result = $id;
+            if($id==NULL){
+                $this->db->insert($this->_table_name,$data);
+                $result = $this->db->insert_id();
+            }else{
+                $this->db->where('orders_id',$id);
+                $this->db->update($this->_table_name,$data);
+                $result = $id;
+            }
+            return $result;
         }
-        return $result;
-    }
+        public function getOrderExtraDetails($id,$property,$propertyValue){
+            $this->db->where('orders_id',$id);
+            $this->db->select()->from('orders_details');
+            $rows =$this->db->get()->num_rows();
+            if($rows>0){
+                $this->db->where('orders_id',$id);
+                $this->db->where($property,$propertyValue);
+                $this->db->select()->from('orders_details');
+                return @$this->db->get()->row()->value;
+            }else{
+                return false;
+            }
+        }
 }
 ?>
