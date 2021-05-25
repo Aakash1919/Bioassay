438bf5eb0616e9417446c121f5d5aff66b99dd0d
Aakash1919 <programmer.aakash@gmail.com>
2021-05-26 01:24:01 +0530

    Added Transaction Model

--- application/models/Member/Transaction_Model.php
+++ application/models/Member/Transaction_Model.php
@@ -10,7 +10,7 @@ class Transaction_Model extends Members_Model{
 	public function getPricebyOrderId($orderId = null) {
 		$this->db->where('orders_id', $orderId);
 		$this->db->select('total_price')->from($this->_table_name);
-		$result = $this->db->get()->results();
+		$result = $this->db->get()->result();
 		return isset($result[0]->total_price) ? $result[0]->total_price : false;
 	}
 }
