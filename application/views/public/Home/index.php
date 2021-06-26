f8c067b2f86d65ebbcf95e4e8eecbe30fcaa5d75
Aakash1919 <programmer.aakash@gmail.com>
2021-06-26 21:01:39 +0530

    Added some changes on home page

--- application/views/public/Home/index.php
+++ application/views/public/Home/index.php
@@ -74,6 +74,18 @@
 			<div class="new-products-list">
 				<ul style="position: relative; top: 2px; left: 20px; width: 270px; padding-top: 10px;">
 					<li>
+						<a href="/Hydroxyproline-Assay-Kit.html">Hydroxyproline Assay Kit<font color="red"><i>*New</i></font></a>
+					</li>
+					<li>
+						<a href="/Salicylate-Assay-Kit.html">Salicylate Assay Kit<font color="red"><i>*New</i></font></a>
+					</li>
+					<li>
+						<a href="/Fatty-Acid-Uptake-Assay-Kit.html">Fatty Acid Uptake Assay Kit<font color="red"><i>*New</i></font></a>
+					</li>
+					<li>
+						<a href="/Detergent-Assay-Kit.html">Detergent Assay Kit<font color="red"><i>*New</i></font></a>
+					</li>
+					<li>
 						<a href="/Hyaluronidase-Inhibitor-Screening-Assay-Kit.html">Hyaluronidase Inhibitor Screening Assay Kit<font color="red"><i>*New</i></font></a>
 					</li>
 					<li>
@@ -85,23 +97,24 @@
 					<li>
 						<a href="/D-Amino-Acid-Assay-Kit.html">D-Amino Acid Assay Kit<font color="red"><i>*New</i></font></a>
 					</li>
-					<li>
-						<a href="/Boron-Assay-Kit.html">Boron Assay Kit<font color="red"><i>*New</i></font></a>
-					</li>
-					<li>
-						<a href="/Biotin-Assay-Kit.html">Biotin Assay Kit<font color="red"><i>*New</i></font></a>
-					</li>
-					<li>
-						<a href="/L-Amino-Acid-Assay-Kit.html">L-Amino Acid Assay Kit<font color="red"><i>*New</i></font></a>
-					</li>
-					<li>
-						<a href="/Oxalate-Quick-Test-Strips.html">Oxalate Quick Test Strips<font color="red"><i>*New</i></font></a>
-					</li>				
+			
 				</ul>
 
 				<ul style="position: relative; top: 2px; left: 20px; width: 270px; padding-top: 10px;">
 					<li>
-						<a href="/Albumin-Quick-Test-Strips.html">Albumin Quick Test Strips<font color="red"><i>*New</i></font></a>
+						<a href="/Boron-Assay-Kit.html">Boron Assay Kit</a>
+					</li>
+					<li>
+						<a href="/Biotin-Assay-Kit.html">Biotin Assay Kit</a>
+					</li>
+					<li>
+						<a href="/L-Amino-Acid-Assay-Kit.html">L-Amino Acid Assay Kit</a>
+					</li>
+					<li>
+						<a href="/Oxalate-Quick-Test-Strips.html">Oxalate Quick Test Strips</a>
+					</li>
+					<li>
+						<a href="/Albumin-Quick-Test-Strips.html">Albumin Quick Test Strips</a>
 					</li>
 					<li>
 						<a href="/PAMPA-Kit.html">PAMPA Kit</a>
@@ -115,15 +128,7 @@
 					<li>
 						<a href="/Gliadin-ELISA-Kit.html">Gliadin ELISA Kit</a>
 					</li>
-					<li>
-						<a href="/Free-Amino-Nitrogen-Assay-Kit-(PFAN-25).html">FAN Assay Kit (PiCO Explorer)</a>
-					</li>
-					<li>
-						<a href="/Histamine-Assay-Kit-(PHIS-25).html">Histamine Assay Kit (PiCO Explorer)</a>
-					</li>
-					<li>
-						<a href="/L-Glutamate-Assay-Kit-(PGLT-25).html">L-Glutamate Assay Kit (PiCO Explorer)</a>
-					</li>
+
 				</ul>
 
 				<ul style="position: relative; top: 2px; left: 20px; width: 180px; padding-top: 10px;">
@@ -195,9 +200,9 @@
 						<li>
 							<a href="/services/lead-discovery-services.html" target="_blank"> Lead Discovery Services</a>
 						</li>
-						<li>
+						<!---<li>
 							<a href="/services/multiplex-assay-services.html" target="_blank"> Multiplex Assay Services</a>
-						</li>
+						</li>-->
 						Please <a href="javascript:location='mailto:\u0073\u0065\u0072\u0076\u0069\u0063\u0065\u0040\u0062\u0069\u006f\u0061\u0073\u0073\u0061\u0079\u0073\u0079\u0073\u002e\u0063\u006f\u006d\u003f\u0073\u0075\u0062\u006a\u0065\u0063\u0074\u003d\u0053\u0065\u0072\u0076\u0069\u0063\u0065\u0020\u0052\u0065\u0071\u0075\u0065\u0073\u0074';void 0">contact us by email</a> or call <a href="tel:18777823888">+1-877-782-3888</a> x 2 to discuss details and feasibility of any potential service projects.
 					</ul>
 				</div>
